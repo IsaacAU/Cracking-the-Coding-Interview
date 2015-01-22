3e56f78324e618d6907323cf089f582dfeb9d18f
Isaac <ryanwoo1989@gmail.com>
2015-01-21 20:23:57 -0500

    Create 8_4.cpp

---
+++ chapter8/8_4.cpp
@@ -0,0 +1,14 @@
+#include<string>
+#include<vector>
+
+using namespace std;
+
+vector<string> permute(string &s){
+  vector<string> res;
+  if(s.empty()) return res;
+  sort(begin(s), end(s));
+  do{
+    res.push_back(s);
+  }while(next_permutation(begin(s), end(s)));
+  return res;
+}
